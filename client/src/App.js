d13984a3f11b5a541166e91e3b87c6f8ecbbca7c
IgnacioCava <cavaignacio11@gmail.com>
2022-05-11 11:40:39 -0300

    Applied changes. Solved register sequence not redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthStore from './store/contexts/AuthStore';
 import Landing from './pages/Landing/Container/Landing';
 import Register from './pages/Landing/AuthForms/Register';
@@ -6,16 +6,15 @@ import Login from './pages/Landing/AuthForms/Login';
 import Directions from './pages/Landing/Directions/Directions';
 import Home from './pages/Home/Home';
 import './App.css';
-import { useEffect } from 'react';
 
 export default function App() {
 
   return (
     <div className="App">
       <Routes>
-        <Route path="*" element={<Navigate to={'/landing'}/>}/>
+        <Route path="*" element={<Navigate to={'/'}/>}/>
         
-        <Route path="/landing" element={<AuthStore><Landing/></AuthStore>}>
+        <Route path="/" element={<AuthStore><Landing/></AuthStore>}>
           <Route index element={<Directions/>}/>
           <Route path="register" element={<Register/>}/>
           <Route path="login" element={<Login/>}/>
