d08199cb07ef0dfd70383576df5ebc65402789a1
IgnacioCava <cavaignacio11@gmail.com>
2022-05-12 08:05:27 -0300

    Removed backend signup validation

--- api/routes/api/auth.js
+++ api/routes/api/auth.js
@@ -13,36 +13,6 @@ router.post('/register', async (req, res) => {
 
     const { username, email, password } = req.body;
 
-    const emailRegexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-  
-    const validation = {
-      email: {test: emailRegexp.test(email), error:'invalid email format'}, 
-      username: {test: username?.length>=3, error: 'username must be at least 3 characters'}, 
-      password: {test: password.length>=6, error:'password must be at least 6 characters'}
-    };
-    
-    const missingFields = [];
-    const emptyFields = [];
-    const validationErrors = []
-    
-    for (let key in { username, email, password }) {
-      if (req.body[key] === '') emptyFields.push(key);
-      if (!req.body.hasOwnProperty(key)) missingFields.push(key);
-      if (!validation[key].test) validationErrors.push(validation[key].error);
-    }
-
-    if(missingFields+emptyFields+validationErrors){
-      return res.status(400).json({ 
-        error: 
-          (missingFields.length?
-          `Missing required fields: ${missingFields.join(', ')+'. '}` : '') 
-        + (emptyFields.length?
-          `Empty fields: ${emptyFields.join(', ')+'. '}` : '')
-        + (validationErrors.length?
-          `Validation errors: ${validationErrors.join(', ')+'.'}` : '')
-        });
-    }
-
     try {
       // See if user exists-----
       if (await User.findOne({ email })) return res.status(400).json({ error: 'User already exists' });
