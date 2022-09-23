74c1b4d18a29f264be905ade53253355221b6fc6
IgnacioCava <cavaignacio11@gmail.com>
2022-09-23 12:49:16 -0300

    fix: cors

--- client/src/settings/apiRequest.js
+++ client/src/settings/apiRequest.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'Access-Control-Allow-Origin' : '*'
-  }
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default baseURL
