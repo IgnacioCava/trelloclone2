2dc2d758bf168c71e69d43c967f95a7b989a9cf4
IgnacioCava <cavaignacio11@gmail.com>
2022-09-23 15:30:38 -0300

    fix: cors

--- api/routes/api/boards.js
+++ api/routes/api/boards.js
@@ -70,7 +70,7 @@ router.get('/:id', auth, async (req, res) => {
 // Get a board's activity
 router.get('/activity', auth, async (req, res) => {
   try {
-    const board = await Board.findById(req.header('boardId')).select('activity');
+    const board = await Board.findById(req.header('boardId'))?.select('activity');
     if (!board) return res.status(404).json({ msg: 'Board not found' });
 
     res.json(board.activity);
